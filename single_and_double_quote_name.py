7d27749aa23dc3e34c01d1dd198cbe38a8d515bf
Francisco <Macbook@News-MacBook-Pro-5.local>
2019-07-06 11:36:31 -0600

    escaping quotes

---
+++ single_and_double_quote_name.py
@@ -0,0 +1,12 @@
+#list1 = ['1', '2', '3']
+#str1 = ''.join(list1)
+#print(str1)
+
+#User inputs first and last name
+input_name = input('Type your first and last name: ')
+
+#Return the splitted name
+return_split = input_name.split(' ')
+
+#Print the split name
+print(return_split)
